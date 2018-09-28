2ea66c0827bfa48926eb7b6bda67fd7e94553d34
s20171106574 <s20171106574@admindeimac-9.local>
2018-09-28 20:08:38 +0800

    Initial Commit

---
+++ test_01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test_01
+//
+//  Created by s20171106574 on 2018/9/28.
+//  Copyright © 2018年 s20171106574. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
